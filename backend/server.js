6e34c2df074141826e809cd95f4b29bc71659009
pasan.mahela <152671109+pasanMahela@users.noreply.github.com>
2024-08-29 20:34:52 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
 app.listen(PORT, () => {
